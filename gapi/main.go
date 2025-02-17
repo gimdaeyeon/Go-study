00e51131532a8d3e4f827d0c286778f4e824577e
gimdaeyeon <rlaeodus1306@naver.com>
2025-02-17 17:31:16 +0900

    Feat: 환경변수 사용 추가

--- gapi/main.go
+++ gapi/main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// import "github.com/joho/godotenv"
 
 func main() {
+	// .env 파일 로드
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env file: %v", err)
+	}
+
+	// 환경 변수에서 포트 가져오기
+	port := os.Getenv("PORT")
+
+	// go에서 string은 nil이 될 수 없음 따라서 빈 문자열인지만 확인
+	// 추가적으로 확인해야한다면 포트번호에 올 수 있는 숫자인지 확인하면 될 것 같다.(정규표현식 활용)
+	if port == "" {
+		log.Fatalf("PORT is not set in the .env file")
+	}
+
 	app := gin.Default()
 	app.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"hello": "world",
 		})
 	})
-	app.Run("0.0.0.0:8000")
+	app.Run("0.0.0.0:" + port)
 }
