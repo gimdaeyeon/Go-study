9c10eceb4b37dfd6663e00f60e27b2a342296700
Gim DaeYeon <rlaeodus1306@naver.com>
2025-02-27 15:01:48 +0900

    Study: 간단한 url checker 완성

--- learngo/main.go
+++ learngo/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"learngo/mydict"
+	"net/http"
+)
+
+var (
+	errRequestFailed = errors.New("request failed")
 )
 
 func main() {
-	dict := mydict.Dictionary{"first": "First word"}
-	baseWord := "hello"
-	dict.Add(baseWord, "First")
-	dict.Search(baseWord)
-	dict.Delete(baseWord)
+	var results = make(map[string]string)
 
-	word, err := dict.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(word)
 
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, ":", result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
