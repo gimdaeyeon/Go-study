79756aba20d21c01a83f76a3574c7051523a2a73
gimdaeyeon <rlaeodus1306@naver.com>
2025-07-03 23:30:16 +0900

    Study: golang flag 라이브러리, make 관련 내용 학습

--- videoEncoding/main.go
+++ videoEncoding/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
+
+// 기본적인 비디오 인코더를 만드는 방법에 대한 내용
+// 실제 비디오 인코더는 이보다 훨씬 더 복잡하여 99.9% 이상의 압축률을 달성하지만,
+// 이 가이드에서는 간단한 인코더로 90% 압축률을 달성하는 방법을 보여준다.
+// 기본적으로 비디오 인코딩은 이미지 인코딩과 매우 유사하지만, 시간적으로 압축할 수 있다.
+// 이미지 압축은 종종 인간의 눈이 색상의 작은 변화에 둔감하다는 점을 활용하는데,
+// 이 인코더에서도 이 점을 활용한다.
+
+// 또한, 이전 기술을 사용하고 더 많은 수학적 계산이 필요한 최신 기술은 다루지 않는다.
+// 이 프로젝트에서는 "최적의" 인코딩 방식에 얽매이지 않고
+// 비디오 인코딩의 핵심 개념에 집중하기 위함이다.
+
+// 코드를 실행
+//   cat video.rgb24 | go run main.go
+
 
 func main() {
-	fmt.Println("hello world")
+	var width, height int
+
+	// flag 패키지: 명령줄에서 전달된 옵션(플래그)을 정의하고 파싱해서,
+	// 프로그램 안의 변수에 그 값을 할당하도록 돕는 표준 라이브러리
+	flag.IntVar(&width, "width", 384, "width of the video")
+	flag.IntVar(&height, "height", 216, "height of the video")
+	flag.Parse() // Parse() 를 통해서 실제로 cli를 통해 선언한 값이 각 변수에 할당된다.
+
+	frames := make([][]byte, 0 ) // make를 통해 slice생성
+
+
+
+
 }
